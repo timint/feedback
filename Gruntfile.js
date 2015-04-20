403fbb526a85b89005924e8cd82b9ccdd1d36481
R. Diaz de Leon <leon@elinter.net>
2015-04-20 04:29:46 -0500

    Added jshint check to grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,17 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'dist/<%= pkg.name %>.min.js'
       }
+    },
+    jshint: {
+        all: ['src/feedback.js']
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint', 'uglify']);
 
 };
