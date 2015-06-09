34457025474d67e53b3b6b131967f058f2869e2b
R. Diaz de Leon <leon@elinter.net>
2015-06-09 15:11:30 -0500

    Python script to test the plugin

---
+++ src/listener.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, send_from_directory
+from time import sleep
+
+app = Flask(__name__, static_url_path='/public')
+
+@app.route('/')
+def root():
+    return send_from_directory('./', 'index.html')
+
+@app.route('/feedback.js')
+def script():
+    return send_from_directory('./', 'feedback.js')
+
+@app.route('/feedback.css')
+def style():
+    return send_from_directory('./', 'feedback.css')
+
+@app.route('/listener', methods=['GET', 'POST'])
+def listener():
+    sleep(30)
+    return '1'
+
+if __name__ == '__main__':
+    app.run()
