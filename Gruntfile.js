56232bc7227ad45b367541faf91210fff8f94500
Rafael Diaz de Leon <leon@elinter.net>
2015-06-10 00:19:13 -0500

    Minified CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,14 +14,26 @@ module.exports = function(grunt) {
     },
     jshint: {
         all: ['src/feedback.js']
+    },
+    cssmin: {
+        target: {
+            files: [{
+                expand: true,
+                cwd: 'src',
+                src: ['*.css', '!*.min.css'],
+                dest: 'dist',
+                ext: '.min.css'
+            }]
+        }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
 
 };
